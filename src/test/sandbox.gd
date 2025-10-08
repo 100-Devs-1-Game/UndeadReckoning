009ff19e9d3ac5fa79681cfc6caadc8a22522e9b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-08 11:09:44 +0200

    Sandbox with configurable wind gust chance

--- src/test/sandbox.gd
+++ src/test/sandbox.gd
@@ -2,10 +2,11 @@ extends Node
 
 @export var aircraft: Aircraft
 
+@export var wind_gust_chance: float= 5.0
 
 func _physics_process(delta: float) -> void:
-	if randf() < 0.005:
+	if randf() < wind_gust_chance / 100.0:
 		var impulse:= RngUtils.get_random_vec3()
 		var position:= RngUtils.get_random_vec3()
 		
-		aircraft.apply_impulse(impulse * 100, position)
+		aircraft.apply_force(impulse * 20, position * 5)

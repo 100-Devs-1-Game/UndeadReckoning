af943377e9b095989a7857e403166d268fe33f2c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-10 14:45:55 +0200

    Add placeholder cockpit

---
+++ src/plane/cockpit.gd
@@ -0,0 +1,4 @@
+class_name Cockpit
+extends Node3D
+
+@export var aircraft: OurAircraft
---
+++ src/plane/instrument.gd
@@ -0,0 +1,26 @@
+class_name Instrument
+extends Node3D
+
+@export var cockpit: Cockpit
+@export var connect_to_module: String
+
+
+func _ready() -> void:
+	cockpit.aircraft.setup_finished.connect(connect_module)
+
+
+func connect_module():
+	var module= cockpit.aircraft.find_modules_by_type(connect_to_module)[0]
+	var generic_module: AircraftModule
+	var spatial_module: AircraftModuleSpatial
+	
+	if module is AircraftModule:
+		generic_module= module
+		generic_module.update_interface.connect(_on_update_interface)
+	elif module is AircraftModuleSpatial:
+		spatial_module= module
+		spatial_module.update_interface.connect(_on_update_interface)
+
+
+func _on_update_interface(values: Dictionary):
+	pass

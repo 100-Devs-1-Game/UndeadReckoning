fd763e2b63cb574d0b724edd1d177f0cdb6f6c11
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-14 03:34:08 +0200

    Toggle built-in interface

--- src/plane/our_aircraft.gd
+++ src/plane/our_aircraft.gd
@@ -1,5 +1,11 @@
 class_name OurAircraft
 extends Aircraft
 
+@export var enable_builtin_interface: bool= false
 
 @onready var builtin_interface: CanvasLayer = $"Builtin Interface"
+
+
+func _ready() -> void:
+	super()
+	builtin_interface.visible= enable_builtin_interface

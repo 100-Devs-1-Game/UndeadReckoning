23850e29f8c4a181a02811ec7ad7ddc1fdaed4b9
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-14 07:14:23 +0200

    Smooth out steering handle

--- src/plane/cockpit.gd
+++ src/plane/cockpit.gd
@@ -6,8 +6,15 @@ extends Node3D
 @export var steering_joint: Node3D
 @export var steering_handle_joint: Node3D
 
+var target_steering_joint:= Node3D.new()
+var target_steering_handle_joint:= Node3D.new()
+
+
 
 func _ready() -> void:
+	target_steering_joint.basis= steering_joint.basis
+	target_steering_handle_joint.basis= steering_handle_joint.basis
+	
 	await aircraft.setup_finished
 	
 	var steering_module: AircraftModule= aircraft.find_modules_by_type("steering")[0]
@@ -15,11 +22,11 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	#steering_joint.basis= steering_joint.basis.slerp(Basis.IDENTITY, delta)
-	pass
+	steering_joint.basis= steering_joint.basis.slerp(target_steering_joint.basis, delta).orthonormalized()
+	steering_handle_joint.basis= steering_handle_joint.basis.slerp(target_steering_handle_joint.basis, delta).orthonormalized()
+
 
 func _on_update_steering(values: Dictionary):
-	steering_joint.rotation_degrees.x= values["axis_x"] * 10
-	steering_joint.rotation_degrees.z= values["axis_y"] * 10
-	steering_handle_joint.rotation_degrees.z= -values["axis_z"] * 10
-	
+	target_steering_joint.rotation_degrees.x= values["axis_x"] * 10
+	target_steering_joint.rotation_degrees.z= values["axis_y"] * 10
+	target_steering_handle_joint.rotation_degrees.z= -values["axis_z"] * 10

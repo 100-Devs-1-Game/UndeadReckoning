ae024ad7a8aff7cec3cdb482df6185b4647249b2
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-15 13:16:24 +0200

    Steering input limit

--- src/addons/simplified_flightsim/aircraft_modules/Controls/ControlSteering.gd
+++ src/addons/simplified_flightsim/aircraft_modules/Controls/ControlSteering.gd
@@ -7,6 +7,7 @@ class_name AircraftModule_ControlSteering
 
 @export var ControlActive: bool = true
 @export var enable_mouse_steering: bool = true
+@export var input_limit: float= 0.5
 @export var mouse_sensitivity: float = 0.1
 
 # There should be only one steering and one steering control in the aircraft
@@ -25,6 +26,8 @@ func receive_input(event):
 	if (not steering_module) or (not ControlActive):
 		return
 	
+	var factor:= input_limit
+	
 	if (event is InputEventKey) and (not event.echo):
 		
 		var axis_z = 0.0
@@ -33,7 +36,7 @@ func receive_input(event):
 		if Input.is_key_pressed(KEY_D):
 			axis_z += 1.0
 		
-		steering_module.set_z(axis_z)
+		steering_module.set_z(clampf(axis_z, -factor, factor))
 		
 		var axis_x = 0.0
 		if Input.is_key_pressed(KEY_W):
@@ -41,7 +44,7 @@ func receive_input(event):
 		if Input.is_key_pressed(KEY_S):
 			axis_x += 1.0
 		
-		steering_module.set_x(axis_x)
+		steering_module.set_x(clampf(axis_x, -factor, factor))
 		
 		# Y axis positive turns plane left
 		var axis_y = 0.0
@@ -50,8 +53,8 @@ func receive_input(event):
 		if Input.is_key_pressed(KEY_E):
 			axis_y -= 1.0
 		
-		steering_module.set_y(axis_y)
+		steering_module.set_y(clampf(axis_y, -factor, factor))
 	
 	elif event is InputEventMouseMotion and enable_mouse_steering:
-		steering_module.set_x(event.relative.y * mouse_sensitivity)
-		steering_module.set_z(event.relative.x * mouse_sensitivity)
+		steering_module.set_x(clampf(event.relative.y * mouse_sensitivity, -factor, factor))
+		steering_module.set_z(clampf(event.relative.x * mouse_sensitivity, -factor, factor))

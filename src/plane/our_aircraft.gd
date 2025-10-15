bfa0aac9a05d52a2cbef5dee354a507bfb3229a2
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-15 13:21:30 +0200

    Connect model ailerons to steering

--- src/plane/our_aircraft.gd
+++ src/plane/our_aircraft.gd
@@ -12,3 +12,5 @@ func _ready() -> void:
 	Input.mouse_mode= Input.MOUSE_MODE_CAPTURED
 	
 	builtin_interface.visible= enable_builtin_interface
+
+	$Steering.update_interface.connect($Model/MovingParts/Steering._on_Steering_update_interface)

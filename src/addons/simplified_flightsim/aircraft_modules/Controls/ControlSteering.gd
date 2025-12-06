b521c6b9475a39c000faf34804175c4e93af78e1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-06 02:53:26 +0100

    Input limits per axis

--- src/addons/simplified_flightsim/aircraft_modules/Controls/ControlSteering.gd
+++ src/addons/simplified_flightsim/aircraft_modules/Controls/ControlSteering.gd
@@ -7,7 +7,7 @@ class_name AircraftModule_ControlSteering
 
 @export var ControlActive: bool = true
 @export var enable_mouse_steering: bool = true
-@export var input_limit: float= 0.5
+@export var input_limit:= Vector3(0.5, 0.3, 0.1)
 @export var mouse_sensitivity: float = 0.1
 
 # There should be only one steering and one steering control in the aircraft
@@ -31,8 +31,6 @@ func receive_input(event):
 	if (not steering_module) or (not ControlActive):
 		return
 	
-	var factor:= input_limit
-	
 	if (event is InputEventKey) and (not event.echo):
 		
 		var axis_z = 0.0
@@ -41,7 +39,7 @@ func receive_input(event):
 		if Input.is_key_pressed(KEY_D):
 			axis_z += 1.0
 		
-		steering_module.set_z(clampf(axis_z, -factor, factor))
+		steering_module.set_z(clampf(axis_z, -input_limit.z, input_limit.z))
 		
 		var axis_x = 0.0
 		if Input.is_key_pressed(KEY_W):
@@ -49,7 +47,7 @@ func receive_input(event):
 		if Input.is_key_pressed(KEY_S):
 			axis_x += 1.0
 		
-		steering_module.set_x(clampf(axis_x, -factor, factor))
+		steering_module.set_x(clampf(axis_x, -input_limit.x, input_limit.x))
 		
 		# Y axis positive turns plane left
 		var axis_y = 0.0
@@ -58,16 +56,18 @@ func receive_input(event):
 		if Input.is_key_pressed(KEY_E):
 			axis_y -= 1.0
 		
-		steering_module.set_y(clampf(axis_y, -factor, factor))
+		steering_module.set_y(clampf(axis_y, -input_limit.y, input_limit.y))
 	
 		if Input.is_key_pressed(KEY_R):
 			toggle_pitch_lock()
 	
 	elif event is InputEventMouseMotion and enable_mouse_steering:
 		mouse_steer_target+= Vector2(event.relative.y, event.relative.x) * mouse_sensitivity
-		mouse_steer_target= mouse_steer_target.limit_length(factor)
-		steering_module.set_x(pow(mouse_steer_target.x / factor, 5) * factor)
-		steering_module.set_z(pow(mouse_steer_target.y / factor, 5) * factor)
+		#mouse_steer_target= mouse_steer_target.limit_length(input_limit)
+		mouse_steer_target.x= clampf(mouse_steer_target.x, -input_limit.x, input_limit.x)
+		mouse_steer_target.y= clampf(mouse_steer_target.y, -input_limit.z, input_limit.z)
+		steering_module.set_x(pow(mouse_steer_target.x / input_limit.x, 5) * input_limit.x)
+		steering_module.set_z(pow(mouse_steer_target.y / input_limit.z, 5) * input_limit.z)
 	
 	elif event is InputEventMouseButton:
 		if not event.pressed:
@@ -84,7 +84,7 @@ func receive_input(event):
 func process_physic_frame(_delta):
 	if is_pitch_locked and not is_equal_approx(locked_pitch, aircraft.instrument_attitude.current_pitch):
 		var pitch_delta: float= locked_pitch - aircraft.instrument_attitude.current_pitch
-		steering_module.set_x(clampf(sqrt(abs(pitch_delta) * 2) * sign(pitch_delta), -1.0, 1.0 ))
+		steering_module.set_x(clampf(pitch_delta * 5, -input_limit.x, input_limit.x))
 
 
 func toggle_pitch_lock():

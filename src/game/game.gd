3826fc764a6354b4b98317e664efd46bfa151893
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-02-02 09:56:59 +0100

    Camera update

--- src/game/game.gd
+++ src/game/game.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 
 @export var terrain_provider: TerrainProvider
-@export var cockpit_view: bool= false
+@export var cockpit_view: bool= true
 
 @export var tripod: CameraTripod
 @export var aircraft: OurAircraft
@@ -15,7 +15,8 @@ var cameras: Array[Camera3D]
 
 
 func _ready() -> void:
-	setup_camera()
+	#update_camera()
+	pass
 
 func _input(event: InputEvent) -> void:
 	if not event.is_pressed():
@@ -26,15 +27,8 @@ func _input(event: InputEvent) -> void:
 			var mode:= DisplayServer.window_get_mode(0)
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if mode == DisplayServer.WINDOW_MODE_WINDOWED else DisplayServer.WINDOW_MODE_FULLSCREEN)
 		if event.keycode == KEY_C:
-			for camera in cameras:
-				if not camera or not is_instance_valid(camera):
-					continue
-				if camera.current:
-					var idx:= cameras.find(camera)
-					idx= wrapi(idx + 1, 0, cameras.size())
-					cameras[idx].current= true
-					aircraft.cockpit.visible= camera != aircraft.cockpit.camera
-					break
+			cockpit_view= not cockpit_view
+			update_camera()
 
 
 func spawn_aircraft(pos: Vector3, snap_to_ground: bool= false, plane_basis: Basis= Basis.IDENTITY):
@@ -47,21 +41,18 @@ func spawn_aircraft(pos: Vector3, snap_to_ground: bool= false, plane_basis: Basi
 	aircraft.crashed.connect(_on_aircraft_crashed)
 	add_child(aircraft)
 	EventBus.aircraft_spawned.emit(aircraft)
-	setup_camera()
+	update_camera()
 
 
-func setup_camera():
+func update_camera():
 	var camera: Camera3D
 	if aircraft:
 		camera= aircraft.cockpit.camera
-		cameras.append(camera)
-		aircraft.cockpit.show()
+		aircraft.cockpit.visible= cockpit_view
 		camera.current= cockpit_view
 		if tripod:
 			camera= tripod.camera
-			cameras.append(camera)
 			camera.current= not cockpit_view
-			aircraft.cockpit.hide()
 
 
 func _on_aircraft_crashed(_impact_velocity: Variant) -> void:

293d63545764af7d32ce6cd465be7312813cb2f0
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-02-02 10:51:31 +0100

    Update rng_utils.gd

--- src/global/rng_utils.gd
+++ src/global/rng_utils.gd
@@ -1,4 +1,13 @@
 class_name RngUtils
 
+
+static func chance100(c: float)-> bool:
+	return chancef(c / 100.0)
+
+
+static func chancef(c: float)-> bool:
+	return randf() < c
+
+
 static func get_random_vec3()-> Vector3:
 	return Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1))

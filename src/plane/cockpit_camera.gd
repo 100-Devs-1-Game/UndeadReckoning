1fcb4f35cc474167443bde15af364e9e91077345
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-02-02 10:51:17 +0100

    Implement rolling rumble

---
+++ src/plane/cockpit_camera.gd
@@ -0,0 +1,20 @@
+extends Camera3D
+
+@export var return_strength: float= 10
+
+
+
+func _ready() -> void:
+	var cockpit: Cockpit= get_parent()
+	await cockpit.ready
+	cockpit.aircraft.rumble.connect(on_rumble)
+
+
+func _process(delta: float) -> void:
+	h_offset= lerp(h_offset, 0.0, delta * return_strength)
+	v_offset= lerp(v_offset, 0.0, delta * return_strength)
+
+
+func on_rumble(vec: Vector2):
+	h_offset= vec.x
+	v_offset= vec.y
--- src/plane/our_aircraft.gd
+++ src/plane/our_aircraft.gd
@@ -1,6 +1,8 @@
 class_name OurAircraft
 extends Aircraft
 
+signal rumble(vec: Vector2)
+
 @export var enable_builtin_interface: bool= false
 @export var cockpit: Cockpit
 
@@ -25,6 +27,12 @@ func _physics_process(delta):
 	super(delta)
 	vertical_speed= air_velocity_vector.y
 
+	if not is_flying() and RngUtils.chance100(air_velocity):
+		var strength: float= air_velocity / 1000.0
+		rumble.emit(Vector2(0, randf_range(-strength, strength)))
+
+func is_flying()-> bool:
+	return get_contact_count() == 0
 
 func _on_control_steering_pitch_lock_toggled(toggled_on: bool) -> void:
 	cockpit.pitch_lock_toggled.emit(toggled_on)

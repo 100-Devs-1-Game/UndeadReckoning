df8178c28b3ef02aced5d9d1caf65b0c0ae23169
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-02-05 22:14:46 +0100

    Implement debug disable tutorial

--- src/tutorial/tutorial_level.gd
+++ src/tutorial/tutorial_level.gd
@@ -1,18 +1,23 @@
 extends Game
 
+@export var debug_disable_tutorial:= false
 @export var tutorial_chain: TutorialChain 
 
 
 func _ready() -> void:
 	super()
-	tutorial_chain.segments[0].finished.connect(on_intro_finished)
-	tutorial_chain.start()
+	if not debug_disable_tutorial:
+		tutorial_chain.segments[0].finished.connect(on_intro_finished)
+		tutorial_chain.start()
+	else:
+		on_intro_finished()
 
 
 func on_intro_finished():
 	spawn_aircraft(Vector3.ZERO, true)
 	await aircraft.setup_finished
-	tutorial_chain.advance()
+	if not debug_disable_tutorial:
+		tutorial_chain.advance()
 
 
 func _on_aircraft_crashed(_impact_velocity: Variant) -> void:

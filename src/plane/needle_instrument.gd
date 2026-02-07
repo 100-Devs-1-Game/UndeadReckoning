0037b0bcc0d4413c1eefa16aa800c97c5414c052
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-02-07 13:13:42 +0100

    Add factor to instruments

--- src/plane/needle_instrument.gd
+++ src/plane/needle_instrument.gd
@@ -6,6 +6,7 @@ extends Instrument
 @export var max_rotation: float= 250
 @export var min_value: float= 0
 @export var max_value: float= 100
+@export var factor: float= 1.0
 @export var reverse: bool= false
 
 
@@ -28,11 +29,11 @@ func update_needle(p_needle: Node3D, values: Dictionary, value_factor: float= 1.
 	var max_rot= max_rotation
 
 	var val: float
-	
 	if connect_to_module:
 		val= values[display_value]
 	else:
 		val= cockpit.aircraft.get(display_value)
+	val*= factor
 	
 	var deg: float= remap(val * value_factor, min_value, max_value, min_rot, max_rot)
 	p_needle.rotation.y= deg_to_rad(deg) * ( -1 if reverse else 1 )
